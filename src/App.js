a89c8dee513aee4f1e8bf1196fafdcb55d8165ea
Luis <lar_8912@hotmail.com>
2024-09-07 20:31:11 -0600

    Subida dos

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
-import CrudApi from './componentes/CrudApi'
-import Menu from './componentes/Menu'
-
-
-
-import {Routes, Route} from "react-router-dom";
-
-
-import  './App.css'
-
+import CrudApi from './componentes/CrudApi';
+import Menu from './componentes/Menu';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
 
 function App() {
   return (
     <>
-    <div className="container-fluid p-3 d-flex flex-row ">
-
-    <Menu></Menu>
-    <div className="container d-flex justify-content-center">
-    <div className="col-auto">
-    <Routes>
-
-    <Route path='/' element={<CrudApi></CrudApi>}></Route>
-    <Route path='/*' element={<CrudApi></CrudApi>}></Route>
-    <Route path='/pokemones' element={<CrudApi></CrudApi>}></Route>
-    <Route path='/contador' element={<CrudApi></CrudApi>}></Route>
-    <Route path='/formulario' element={<CrudApi></CrudApi>}></Route>
-    <Route path='/formulario2' element={<CrudApi></CrudApi>}></Route>
-    <Route path='/modales' element={<CrudApi></CrudApi>}></Route>
-    <Route path='/todolist' element={<CrudApi></CrudApi>}></Route>
-    </Routes>
-    </div>
-    </div>
-    </div>
+      <div className="container-fluid p-3 d-flex flex-row ">
+        <Menu></Menu>
+        <div className="container d-flex justify-content-center">
+          <div className="col-auto">
+            <Router>
+              <Routes>
+                <Route path='/' element={<CrudApi />}></Route>
+                <Route path='/*' element={<CrudApi />}></Route>
+                <Route path='/pokemones' element={<CrudApi />}></Route>
+                <Route path='/contador' element={<CrudApi />}></Route>
+                <Route path='/formulario' element={<CrudApi />}></Route>
+                <Route path='/formulario2' element={<CrudApi />}></Route>
+                <Route path='/modales' element={<CrudApi />}></Route>
+                <Route path='/todolist' element={<CrudApi />}></Route>
+              </Routes>
+            </Router>
+          </div>
+        </div>
+      </div>
     </>
-    
-
-  )
+  );
 }
 
-export default App
+export default App;
